b30acae10874814d30146bac7f5a6b467499156c
Andy Shi <shi110@purdue.edu>
2015-04-16 23:20:12 -0400

    Completed basic layout

--- Andy Shi/Andy Shi/ViewController.swift
+++ Andy Shi/Andy Shi/ViewController.swift
@@ -8,28 +8,89 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var pageVC: UIPageViewController!
+class ViewController: UIViewController, UIPageViewControllerDataSource {
 
+    var pageVC: UIPageViewController!
+    var views: NSArray!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //let about : UIViewController = storyboard?.instantiateViewControllerWithIdentifier("AboutView") as UIViewController;
-        //let project = self.storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as! UIPageViewController
-        //var views = ["AboutView", "ProjectView"]
         
-        // Do any additional setup after loading the view, typically from a nib.
-        self.pageVC = self.storyboard?.instantiateViewControllerWithIdentifier("startPage") as! UIPageViewController
+        self.pageVC = self.storyboard?.instantiateViewControllerWithIdentifier("StartPage") as! UIPageViewController
+        self.pageVC.dataSource = self
+        
+        var currentView = self.getViewAtIndex(0)
+        var nextPage = NSArray(object: currentView)
+        self.pageVC.setViewControllers(nextPage as [AnyObject], direction: .Forward, animated: true, completion: nil)
+        
+        //self.pageVC.view.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.size.height - 60)
+        self.pageVC.view.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.size.height)
+        
+        self.addChildViewController(self.pageVC)
+        self.view.addSubview(self.pageVC.view)
+        self.pageVC.didMoveToParentViewController(self)
     }
     
-    @IBAction func changeView(sender: AnyObject) {
-        let vc = storyboard?.instantiateViewControllerWithIdentifier("AboutView") as! UIViewController
-        self.presentViewController(vc, animated: true, completion: nil)
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    
+    // Data Source
+    
+    func getViewAtIndex(index: Int) -> UIViewController
+    {
+        // Declare views
+        //let about   = storyboard?.instantiateViewControllerWithIdentifier("AboutView") as! UIViewController
+        //let project =  as! UIViewController
+        views = ["StartView", "AboutView", "ExpView", "ProjectView", "InterestView", "EndView"]
+    
+        var vc: pages = storyboard?.instantiateViewControllerWithIdentifier(views[index] as! String) as! pages
+        vc.pageIndex = index
+        println(index)
+        //return (views[index] as! UIViewController)
+        return vc
+    }
+
+    func pageViewController(pageVC: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?
+    {
+        var vc = viewController as! pages
+        var index = vc.pageIndex as Int
+
+        if(index == 0)
+        {
+            return nil
+        }
+        index--
+        return self.getViewAtIndex(index)
+    }
+    
+    func pageViewController(pageVC: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController?
+    {
+        var vc = viewController as! pages
+        var index = vc.pageIndex as Int
+        
+        if (index == NSNotFound || index == views.count - 1)
+        {
+            return nil
+        }
+        index++
+       /*
+        if (index == views.count)
+        {
+            return nil
+        }*/
+        return self.getViewAtIndex(index)
+    }
+    
+    
+    func presentationCountForPageViewController(pageVC: UIPageViewController) -> Int {
+        return self.views.count
+    }
+    
+    func presentationIndexForPageViewController(pageVC: UIPageViewController) -> Int {
+        return 0
     }
 
 
